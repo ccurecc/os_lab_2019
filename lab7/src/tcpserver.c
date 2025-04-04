f123216b637e3fe12ae8978b195ab0ca7f9c1772
ccurecc <arseniybeagle@gmail.com>
2025-04-04 20:24:16 +0000

    айпишки забыл

--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -1,62 +1,55 @@
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#define SERV_PORT 10050
 #define BUFSIZE 100
 #define SADDR struct sockaddr
+#define SIZE sizeof(struct sockaddr_in)
 
-int main() {
-  const size_t kSize = sizeof(struct sockaddr_in);
+int main(int argc, char *argv[]) {
+    int fd;
+    int nread;
+    char buf[BUFSIZE];
+    struct sockaddr_in servaddr;
 
-  int lfd, cfd;
-  int nread;
-  char buf[BUFSIZE];
-  struct sockaddr_in servaddr;
-  struct sockaddr_in cliaddr;
-
-  if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("socket");
-    exit(1);
-  }
-
-  memset(&servaddr, 0, kSize);
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(SERV_PORT);
-
-  if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
-    perror("bind");
-    exit(1);
-  }
-
-  if (listen(lfd, 5) < 0) {
-    perror("listen");
-    exit(1);
-  }
-
-  while (1) {
-    unsigned int clilen = kSize;
-
-    if ((cfd = accept(lfd, (SADDR *)&cliaddr, &clilen)) < 0) {
-      perror("accept");
-      exit(1);
-    }
-    printf("connection established\n");
-
-    while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
-      write(1, &buf, nread);
+    if (argc < 3) {
+        printf("Usage: %s <IP address> <port>\n", argv[0]);
+        exit(1);
     }
 
-    if (nread == -1) {
-      perror("read");
-      exit(1);
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket creating");
+        exit(1);
     }
-    close(cfd);
-  }
+
+    memset(&servaddr, 0, SIZE);
+    servaddr.sin_family = AF_INET;
+
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+        perror("bad address");
+        exit(1);
+    }
+
+    servaddr.sin_port = htons(atoi(argv[2]));
+
+    if (connect(fd, (SADDR *)&servaddr, SIZE) < 0) {
+        perror("connect");
+        exit(1);
+    }
+
+    write(1, "Input message to send\n", 22);
+    while ((nread = read(0, buf, BUFSIZE)) > 0) {
+        if (write(fd, buf, nread) < 0) {
+            perror("write");
+            exit(1);
+        }
+    }
+
+    close(fd);
+    exit(0);
 }
